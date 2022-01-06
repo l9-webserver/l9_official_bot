31cfdd366bb2efda8a302ae208685f428e9af9ba
l9-webserver <l9-webserver@hotmail.com>
2022-01-06 20:59:26 +0100

    flask

--- main.py
+++ main.py
@@ -16,7 +16,21 @@ from discord.member import Member
 from discord.message import Message
 from discord.permissions import Permissions
 from discord.role import Role
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+
+t = Thread(target=run)
+t.start()
 
 class json_util:
     def readls(json_file: str) -> str:
